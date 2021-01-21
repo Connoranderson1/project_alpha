e5df48715f6f1774136d59b2fda6a77900410059
Connor Anderson <connoranderson1@ufl.edu>
2021-01-21 14:01:29 -0500

    Added BMI Classification if statements, overcame issues

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -4,7 +4,8 @@ const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let results = calculateMetricBMI(parseInt(weightInput.value), parseInt(heightInput.value))
+    let message = getBMIClassification(parseFloat(results))
     let displayResultsElement = document.getElementById('results')
-    displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results}</h1>`
-    
+    displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results} and you are deemed ${message}</h1>`
+ 
 }
